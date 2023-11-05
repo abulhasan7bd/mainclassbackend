2c1b158f41fddb4c7d50fbaf03eb29e64abb8d81
abul <abulhasan7bd.gmail.com>
2023-11-05 21:58:46 +0600

    vvvvvvvvvvvv

--- src/server.js
+++ src/server.js
@@ -4,35 +4,11 @@ const app = require('./database/app')
 const { port } = require('../src/config/index')
 // eslint-disable-next-line no-undef
 
-
-
-
-
-
-
-
-
-
-
 const { connectDb } = require('./database/connectDb')
 connectDb()
 
 // eslint-disable-next-line no-loss-of-precision
 let apa = 333333333333333333333
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 let hi = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 let sumi = 'tum'
