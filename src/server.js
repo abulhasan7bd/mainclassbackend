d808ef955dc914f05abbc3f4789534a454fe87d9
abul <abulhasan7bd.gmail.com>
2023-11-05 21:28:08 +0600

    tobea

--- src/server.js
+++ src/server.js
@@ -1,26 +1,9 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-loss-of-precision */
-/* eslint-disable no-undef */
-// eslint-disable-next-line no-undef
 const app = require('./database/app')
 const { port } = require('../src/config/index')
 const { connectDb } = require('./database/connectDb')
 connectDb()
 
-let x = 333333333333333333333
-
-
-
-
-
-
-
-
-
-
-
-
-
+let apa = 333333333333333333333
 
 const data = 'dslklsd'
 
