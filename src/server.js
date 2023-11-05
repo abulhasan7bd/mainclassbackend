9e946b3e446d8932b17b6d0b07db21607325389a
abul <abulhasan7bd.gmail.com>
2023-11-05 21:37:38 +0600

    power

--- src/server.js
+++ src/server.js
@@ -1,30 +1,31 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 const app = require('./database/app')
 const { port } = require('../src/config/index')
+// eslint-disable-next-line no-undef
 const { connectDb } = require('./database/connectDb')
 connectDb()
 
+// eslint-disable-next-line no-loss-of-precision
 let apa = 333333333333333333333
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 let l = 3333333333333333
 const data = 'dslklsd'
+
+
+
+
+
+
+
+
+const loo = 34
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
